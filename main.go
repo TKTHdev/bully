c81209affbfedd3f5f6cf19ce081f4b170ca1f33
Harunari Takata <tkt0430@keio.jp>
2025-09-24 01:43:08 +0900

    Made the conf scanning mechanism & taskfile

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type (
@@ -23,6 +27,41 @@ type Node struct {
 	processList []string
 }
 
+func readClusterConfig(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var processList []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line != "" {
+			processList = append(processList, line)
+		}
+	}
+	return processList, scanner.Err()
+}
+
 func main() {
-	fmt.Println("bully")
+	p := &Node{}
+	processList, err := readClusterConfig("cluster.conf")
+	if err != nil {
+		fmt.Println("Error reading cluster config:", err)
+		return
+	}
+	p.processList = processList
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <node_index>")
+		return
+	}
+	index, err := strconv.Atoi(os.Args[1])
+	if err != nil || index < 0 || index >= len(processList) {
+		fmt.Println("Invalid node index")
+		return
+	}
+	p.addr = processList[index]
+	fmt.Println("Node address:", p.addr)
 }
